265bcaf62a7328e6825094155b9ce8d51cef17a4
Amal LEBDIOUA DEMERY <lebdioua.amal@gmail.com>
2023-12-11 16:37:03 +0100

    Modifications

--- scripts/views/modalLightBox.js
+++ scripts/views/modalLightBox.js
@@ -87,10 +87,11 @@ class ModalLightBox extends Modal {
 		const modal = document.querySelector('.lightBox');
 		const lightBoxContent = modal.querySelector('.lightBoxContent');
 		const titre = modal.querySelector('h2');
+		
+		console.log(this.listeMedias[this.position].getMediaFactory(this.photograph));
 
 		titre.textContent = this.listeMedias[this.position].getTitre;
 		lightBoxContent.innerHTML = this.listeMedias[this.position].getMediaFactory(this.photograph);
-		console.log(this.listeMedias[temp].getMediaFactory(this.photograph));
 	}
 
 	mediaPrecedent() {
